28b85b0edd603dc5758e467fa8460039538bcbbf
JamesLDeveloper <109963510+JamesLDeveloper@users.noreply.github.com>
2023-01-29 15:34:53 +0000

    Test JUnit Test to see if uploads correctly to gitHub

--- Medieval Serialization Project Attempt 4/src/ArmourFiles/TestArmour/ArmourTest.java
+++ Medieval Serialization Project Attempt 4/src/ArmourFiles/TestArmour/ArmourTest.java
@@ -1,4 +1,22 @@
+package ArmourFiles.TestArmour;
+
+import ArmourFiles.Armour;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class TestArmourTest {
-  
+
+class ArmourTest {
+
+    @Test
+    public void reduceDurabilityTest(){
+        Armour platedArmour = new Armour("Plated Armour", 10, 5);
+
+     platedArmour.reduceDurability(15);
+
+     assertEquals(0, platedArmour.getDurability());
+
+
+    }
+
+
 }
